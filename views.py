0a0874c147471b41d943f8441b7ec148f4b94186
Andrés Ávila <andaviaco@gmail.com>
2017-05-16 16:58:26 -0500

    build login form

--- views.py
+++ views.py
@@ -1,7 +1,18 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 
 from reporter import app
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        pass
+    else:
+        return redirect(url_for('index'))
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return redirect(url_for('index'))
