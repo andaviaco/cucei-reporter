a4ee1d8626ffe3ac550c67dbe2727de727b43b0f
Andrés Ávila <andaviaco@gmail.com>
2017-05-17 03:00:27 -0500

    update views to add login functionality

--- views.py
+++ views.py
@@ -1,21 +1,39 @@
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect, url_for, flash
+from flask_login import login_user, login_required, current_user
 
 from reporter import app
+from models import User
 
 @app.route('/')
 def index():
+    if current_user.is_authenticated:
+        return redirect(url_for('report'))
     return render_template('index.html')
 
-@app.route('/login/', methods=['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        return redirect(url_for('report'))
+        code = request.form.get('password')
+        user = User.query.filter_by(code=code).first()
+
+        if user is not None:
+            login_user(user)
+            flash('Logged in successfully')
+            return redirect(url_for('report'))
+
+        flash('Password is invalid' , 'error')
+        return redirect(url_for('index'))
     else:
         return redirect(url_for('index'))
 
-@app.route('/report/')
+@app.route('/report', methods=['GET', 'POST'])
+@login_required
 def report():
-    return render_template('report.html')
+    if request.method == 'POST':
+        print(request.form)
+        return render_template('report.html')
+    else:
+        return render_template('report.html')
 
 @app.errorhandler(404)
 def page_not_found(error):
