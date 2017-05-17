bfb63359e262dcd7fcb4efacfe4d6c042f279c6c
Andrés Ávila <andaviaco@gmail.com>
2017-05-17 02:58:33 -0500

    login setup

--- reporter.py
+++ reporter.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 app = Flask(__name__)
 
@@ -7,7 +8,17 @@ app.config.from_envvar('REPORTER_LOCAL_SETTINGS', silent=True)
 
 db = SQLAlchemy(app)
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "index"
+
 import views
+import models
+
+
+@login_manager.user_loader
+def load_user(id):
+    return models.User.query.get(int(id))
 
 if __name__ == '__main__':
     app.run()
