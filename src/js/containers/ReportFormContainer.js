66b9f38d4cee0f27fc75e823d09dbab3ab550363
Andrés Ávila <andaviaco@gmail.com>
2017-05-18 02:54:09 -0500

    send POST request with axios and show alert to refresh

--- src/js/containers/ReportFormContainer.js
+++ src/js/containers/ReportFormContainer.js
@@ -1,5 +1,7 @@
 import { h,  Component } from 'preact';
 import linkstate from 'linkstate';
+import axios from 'axios';
+import swal from 'sweetalert2';
 
 import ReportForm from '../components/ReportForm';
 
@@ -23,7 +25,20 @@ class ReportFormContainer extends Component {
 
     handleFormSubmit(e) {
         e.preventDefault();
-        console.log(this.state);
+
+        axios.post('/report', this.state)
+            .then(function (response) {
+                swal({
+                    title: '¡Correo enviado!',
+                    text: 'Da click en continuar para llenar un nuevo formulario.',
+                    confirmButtonText: 'Continuar',
+                }).then(() =>{
+                    window.location.reload();
+                });
+            })
+            .catch(function (error) {
+
+            });
     }
 
     handleStudentChange(i, student) {
