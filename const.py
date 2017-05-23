26724e41c5b7162cbb20fd00a026367fd289d4d5
Andrés Ávila <andaviaco@gmail.com>
2017-05-23 13:19:18 -0500

    create file to define constants

---
+++ const.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+
+PRIORITY_MAP = {
+    'low': u'baja',
+    'medium': u'media',
+    'high': u'alta',
+}
+
+SITUATION_MAP = {
+    'nonattendance': u'Asistencia',
+    'academics': u'Académica',
+    'discipline': u'Diciplina',
+    'other': u'Otra',
+}
